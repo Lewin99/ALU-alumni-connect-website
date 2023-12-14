c0d24896df5caaf311ebc4d9039f9456159dd600
Lewin99 <l.ndahinyuk@alustudent.com>
2023-12-14 13:41:34 +0400

    cors configurations

--- Backend/server.js
+++ Backend/server.js
@@ -22,11 +22,23 @@ const conString = process.env.Db_connect;
   }
 })();
 
+const allowedOrigins = ["https://alu-alumni-connect-website.vercel.app/"];
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+};
+
 const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors());
+app.use(cors(corsOptions));
 
 app.use("/api/users", usersRoutes);
 app.use("/api/events", eventsRouter);
